6d0eed9e254b2256297e5c6e4d7678f10f363003
Infocat <infocat2.0@gmail.com>
2022-05-12 15:39:38 -0500

    0.6.1

--- impresion.php
+++ impresion.php
@@ -125,6 +125,37 @@
 		</div>
 
 	</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/vue@3"></script>
+<script>
+	var modalCompras, modalRetiros, liveToast, toolTips;
+	var idUsuario =1;
+	var app=Vue.createApp({
+		data() {
+			return {
+				servidor: 'http://localhost/productosMedicina/api/',
+				//servidor: 'http://perumedical.infocatsoluciones.com/api/',
+				busqueda:'', disponibles:[], pedidos:[], topicos:[], cabecera:[],
+				solicitante:'', idTopico:-1, comentarios:''
+			}
+		},
+		created(){
+			let uri = window.location.search.substring(1); 
+			let params = new URLSearchParams(uri);
+			//console.log(params.get('id'));
+			this.id= params.get('id');
+		},
+		mounted(){
+			
+		},
+		methods:{
+			
+		},
+		computed:{
+			
 
+		}
+	}).mount('#app')
+</script>
 </body>
 </html>
